3ef89338405dcb3d4c0487e2044484770a330c08
RISHABH GUPTA <104566113+Rishabh2503@users.noreply.github.com>
2023-05-27 19:06:40 +0530

    add api + lambda + auth + graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
-
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundColor } from '@/Components/QuoteGenerator/QuoteGenerator'
 
+// Components
+import { BackGroundImage1, BackGroundImage2,FootContainer, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundColor, FontLink, QuoteGeneratorContainer, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle } from '@/Components/QuoteGenerator/QuoteGenerator'
+
+
+//Images
+import cloud1 from "../assest/images/2.png"
+import cloud2 from "../assest/images/cloudy-weather.png"
 
 
 export default function Home() {
+  const [numberOfQuotes,setNumberofQuotes] = useState<Number |null>(0);
+
+
+  
   return (
     <>
       <Head>
@@ -15,7 +25,61 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Background */}
-      <GradientBackgroundColor />
+      <GradientBackgroundColor >
+      {/* Quote Generator Modal Pop-up */}
+      {/* <QuoteGeneratorModal />
+      */}
+
+      {/* Quote Generator  */}
+      <QuoteGeneratorContainer>
+        <QuoteGeneratorInnerCon>
+          <QuoteGeneratorTitle>
+            Daily Inspiration Generator
+          </QuoteGeneratorTitle>
+
+          <QuoteGeneratorSubTitle>
+          Embark on a journey of inspiration with <FontLink href="https://zenquotes.io/" target="_blank" rel="noopener">ZenQuotes API.</FontLink> Let our captivating quote cards ignite your passion and fuel your creative fire.
+          </QuoteGeneratorSubTitle>
+          <GenerateQuoteButton>
+            <GenerateQuoteButtonText>
+              Make a Quote
+            </GenerateQuoteButtonText>
+          </GenerateQuoteButton>
+        </QuoteGeneratorInnerCon>
+      </QuoteGeneratorContainer>
+
+
+
+
+      {/* BackGroundImage */}
+        <BackGroundImage1
+        src = {cloud1}
+        height="200"
+        alt="Cloud image"
+        />
+        <BackGroundImage2
+        src={cloud2}
+        height="200"
+        width="300"
+        alt='cloud image'
+        />
+
+
+        {/* Footer Container */}
+        <FootContainer>
+          <>
+          Quotes Generated : {numberOfQuotes}
+          <br />
+          Developed with 💖 by Rishabh Gupta
+          
+          </>
+        </FootContainer>
+
+       
+
+
+        </GradientBackgroundColor>
     </>
   )
+
 }
